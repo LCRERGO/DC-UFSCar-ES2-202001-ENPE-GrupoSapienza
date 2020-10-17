aa423e787b9b7bf24f62a8f1c457f48d1be57541
tdimhcs <tdimhcs2009@gmail.com>
2020-10-17 20:57:33 -0300

    cria nova classe CSVImporter.java

---
+++ src/main/java/net/sf/jabref/importer/fileformat/CSVImporter.java
@@ -0,0 +1,65 @@
+/*  Copyright (C) 2003-2015 JabRef contributors.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package net.sf.jabref.importer.fileformat;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
+
+import net.sf.jabref.importer.OutputPrinter;
+import net.sf.jabref.model.entry.BibEntry;
+
+/**
+ * Importer for the CSV format.
+ */
+public class CSVImporter extends ImportFormat {
+
+    /**
+     * Return the name of this import format.
+     */
+    @Override
+    public String getFormatName() {
+        return "CSV";
+    }
+
+    /*
+     *  (non-Javadoc)
+     * @see net.sf.jabref.imports.ImportFormat#getCLIId()
+     */
+    @Override
+    public String getCLIId() {
+        return "csv";
+    }
+
+    /**
+     * Check whether the source is in the correct format for this importer.
+     */
+    @Override
+    public boolean isRecognizedFormat(InputStream stream) throws IOException {
+        return false;
+    }
+
+    /**
+     * Parse the entries in the source, and return a List of BibEntry
+     * objects.
+     */
+    @Override
+    public List<BibEntry> importEntries(InputStream stream, OutputPrinter status) throws IOException {
+        return Collections.emptyList();
+    }
+
+}
